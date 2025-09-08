9565b95ffb06826ba590af4711d3c09fa0d2e415
NamEon <143885031+skadjs@users.noreply.github.com>
2025-09-09 02:07:21 +0900

    feat: 전역예외처리 클래스 추가
    - RestController에서 발생하는 예외를 전역적으로 처리하기 위한 클래스를 추가함.

---
+++ src/main/java/com/rookies4/MiniProject2/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.rookies4.MiniProject2.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// 모든 @RestController 에서 발생하는 예외를 전역적으로 처리하는 클래스
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // IllegalArgumentException 예외 발생 시 호출되는 핸들러
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
