a4b5363a92cdda98468cded80966d06451859c15
NamEon <143885031+skadjs@users.noreply.github.com>
2025-09-09 02:07:42 +0900

    feat: jwt filter 추가

---
+++ src/main/java/com/rookies4/MiniProject2/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package com.rookies4.MiniProject2.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // Request Header에서 토큰 꺼내기
+        String jwt = resolveToken(request);
+
+        // validateToken 으로 토큰 유효성 검사
+        if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        // 필터로 요청 전달
+        filterChain.doFilter(request, response);
+    }
+
+    // Request Header 에서 토큰 정보 꺼내기
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
